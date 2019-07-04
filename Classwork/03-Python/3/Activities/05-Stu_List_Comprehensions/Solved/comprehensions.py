09c857ded2180371806c744c3f03f3ba17b98cde
Kendall <kendall.gouldthorpe@gmail.com>
2019-07-04 15:44:36 -0400

    Class 7/02

--- Classwork/03-Python/3/Activities/05-Stu_List_Comprehensions/Solved/comprehensions.py
+++ Classwork/03-Python/3/Activities/05-Stu_List_Comprehensions/Solved/comprehensions.py
@@ -10,3 +10,18 @@ invitations = [
 
 for invitation in invitations:
     print(invitation)
+for row in election_data.values():
+    
+    #calcuate total votes cast
+    total_votes = len(election_data[row[0]])
+    print(total_votes)
+
+    #calculate complete list of candidates
+
+    #calculate total vote per candidate
+
+    #calculate percentages of votes for each candidate
+
+    #calculate winner of the election
+#Read CSV
+
