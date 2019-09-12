6a0a93c938b366d09d40f8cc9d17e6a239c178cb
Kendall <kendall.gouldthorpe@gmail.com>
2019-09-12 19:32:48 -0400

    Project for submittal

--- Classwork/14-Intro-To-JavaScript/3/Activities/02-Stu_D3_Select/Unsolved/static/js/app.js
+++ Classwork/14-Intro-To-JavaScript/3/Activities/02-Stu_D3_Select/Unsolved/static/js/app.js
@@ -2,14 +2,15 @@
 var newGrade = ["Wash", 79];
 
 // Use D3 to select the table
-
+var table = d3.select("table");
 // Use d3 to create a bootstrap striped table
 // http://getbootstrap.com/docs/3.3/css/#tables-striped
 
 // Use D3 to select the table body
-
+var tbody = d3.select("tbody");
 // Append one table row `tr` to the table body
-
+var row = tbody.select("tr");
 // Append one cell for the student name
-
+row.select("td").append(newGrade[0]);
 // Append one cell for the student grade
+row.select("td").append(newGrade[1]);
