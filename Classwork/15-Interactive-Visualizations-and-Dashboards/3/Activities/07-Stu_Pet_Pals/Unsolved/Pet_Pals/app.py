e06924ed3335ea671c99eecf5022586e3227e832
Kendall <kendall.gouldthorpe@gmail.com>
2019-09-28 13:58:48 -0400

    class 9/28

--- Classwork/15-Interactive-Visualizations-and-Dashboards/3/Activities/07-Stu_Pet_Pals/Unsolved/Pet_Pals/app.py
+++ Classwork/15-Interactive-Visualizations-and-Dashboards/3/Activities/07-Stu_Pet_Pals/Unsolved/Pet_Pals/app.py
@@ -13,21 +13,49 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# @TODO: Setup your database here
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/db.sqlite"
+
+db = SQLAlchemy(app)
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64))
+    type = db.Column(db.String(64))
+
+@app.before_first_request
+def setup():
+    # Recreate database each time for demo
+    db.drop_all()
+    db.create_all()
+
+@app.route("/send", methods = ["GET", "POST"])
+def send():
+    if request.method == "POST":
+        nickname = request.form["nickname"]
+        type = request.form["type"]
+
+        pet = Pet(nickname = nickname, type = type)
+        db.session.add(pet)
+        db.session.commit()
+
+        return "Thank you for the information on your pet!"
+    return render_template("form.html")
+
+@app.route("/api/pals")
+def pet_list():
+    results = db.session.query(Pet.nickname, Pet.type).all()
+
+    pets = []
+    for result in results:
+        pets.append({"Name":result[0], "Type": result[1]})
+        return jsonify(pets)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
-# @TODO: Create a route "/send" that handles both GET and POST requests
-# If the request method is POST, save the form data to the database
-# Otherwise, return "form.html"
-
-
-# @TODO: Create an API route "/api/pals" to return data to plot
-
 
 if __name__ == "__main__":
     app.run()
